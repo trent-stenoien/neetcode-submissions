d49eaea4879856bbf36c9aa66b10edac02d1acb0
Trent Stenoien <trent.stenoien@gmail.com>
2026-07-29 15:20:36 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.ts
@@ -0,0 +1,24 @@
+class Solution {
+
+    groupAnagrams(strs: string[]): string[][] {
+
+        // Map<sortedStr, [str1, str2, etc]>
+        const orderedStrs: Map<string, string[]> = new Map();
+        
+        for (let i = 0; i < strs.length; i++) {
+            
+            // Sort the given string to use as a key
+            const sortedStr: string = strs[i].split("").sort().join("");
+
+            // If the key exists, push cur str into the array
+            if (orderedStrs.has(sortedStr))
+                orderedStrs.get(sortedStr).push( strs[i] );
+            // Else create the key and array value
+            else
+                orderedStrs.set(sortedStr, [strs[i]] );
+        }
+
+        // Convert the Map's values into an array of string arrays.
+        return Array.from(orderedStrs.values());
+    }
+}
